adf4693eb0734f524e6f076237665084f5e46867
Thanhloc <Nguyenloc12021999vn@gmail.com>
2022-07-10 19:08:22 +0700

    update

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ mongoose.connect(URI,
 
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'))
+    app.use(express.static('build'))
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
     })
