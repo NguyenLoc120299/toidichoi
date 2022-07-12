4e946ffdff5784ff23f21ae2d959166a56d95cd3
Thanhloc <Nguyenloc12021999vn@gmail.com>
2022-07-12 18:50:12 +0700

    update

--- be/server.js
+++ be/server.js
@@ -3,6 +3,7 @@ const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
 const cookieParser = require('cookie-parser')
+const path = require('path');
 
 const app = express()
 app.use(express.json())
@@ -24,7 +25,7 @@ mongoose.connect(URI,
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('build'))
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'build', 'index.html'))
+        res.sendFile(path.join(__dirname, '/', 'build', 'index.html'))
     })
 }
 
